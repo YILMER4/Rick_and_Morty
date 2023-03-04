3f78bf5a7c876294f6e1f5008b6f4c027a4ee965
yilmer <yilzam228@gmail.com>
2023-03-04 00:46:15 -0500

    React Hooks

--- src/components/favorites/Favorites.jsx
+++ src/components/favorites/Favorites.jsx
@@ -1,14 +1,36 @@
+import { useRef } from "react";
 import { useDispatch} from "react-redux"; 
 import { useSelector } from "react-redux"; //reemplazaria  mapDispatchToProps mediante este hooks
 import { Link } from "react-router-dom";
+import { filterCards, orderCards } from "../../redux/actions";
 
 const Favorites= ()=>{
     // por destructuring a diferencia que en el componente Card 
     const {myFavorites} = useSelector(state=>state);
 
+    const dispatch = useDispatch();
+
+    const filter= useRef(null);
+    const order= useRef (null);
+
+    function handleReset(e){
+        dispatch({type:"RESET"});
+        filter.current.value="";
+        order.current.value="";
+    }
+
     return(
         <div>
             <h2>My Favorites</h2>
+            <select ref={order} onChange={(e)=>dispatch(orderCards(e.target.value))} >
+                {["Ascendente", "Descendente"].map((e,i)=><option value={e} key={i}>{e}</option>)}
+            </select>
+            <select ref={filter} onChange={(e)=>dispatch(filterCards(e.target.value))} >
+                {["Male", "Female","unknown","Genderless"].map((e,i)=><option value={e} key={i}>{e}</option>)}
+            </select>
+            <button value="reset" onclick={handleReset} >
+                Reset
+            </button>
             {
                 myFavorites?.map((character)=>{
                     return(
--- src/redux/reducer.js
+++ src/redux/reducer.js
@@ -13,31 +13,47 @@ const initialState ={// estado global
 const rootReducer= (state=initialState, {type, payload})=>{// {type, payload}=action
     switch (type) { //switch (type)= switch (action.type)
         case ADD_CHARACTER:
+            const addFav =[...state.allCharacters, payload ]
         // [...state.list];//se usa para no modificar el estado original para no pisarlo
             return{
                 ...state,// se usa por si el estado global tiene mas de un elemento no se pierdan
-                myFavorites:[...state.myFavorites, payload] // este payload es el personaje que se va estar agregando
+                myFavorites:[...addFav],
+                allCharacters:[...addFav], // este payload es el personaje que se va estar agregando
             };//action.payload
         case DELETE_CHARACTER:
             // se crea constante para nuevo array
             //filtramos que por cada elemento que tiene en su propiedad en su id
             // solamente me van a quedar guardados en esta constante los que sean 
             // diferentes al payload
-            const favoriteFiltered=  state.myFavorites.filter
-            ( e =>e.id !== payload) // en el payload estamos trayendo un id
+            const deleteFav=  state.allCharacters.filter( e =>e.id !== payload) // en el payload estamos trayendo un id
             return{
                 ...state,
-                myFavorites: favoriteFiltered,
+                myFavorites:[...deleteFav],
+                allCharacters:[...deleteFav],
                 // la manera tradicional es no hacer variable y pegar aca directamente
                 // este codigo state.myFavorites.filter( e =>e.id !== payload)
             }
         case FILTER:
             return{
-
+                ...state,
+                myFavorites: state.allCharacters.filter( e =>e.gender === payload)
             }
         case ORDER:
+            let orderFav;
+            if (payload==="Ascendente") {//filtramos solo con lo que hayamos agregado a favoritos
+                orderFav= state.myFavorites.sort((a,b)=>a.id > b.id?1:-1);
+            }else{
+                orderFav= state.myFavorites.sort((a,b)=>a.id < b.id?1:-1);
+            }
             return{
-            }    
+                ...state,
+                myFavorites: [...orderFav],
+            }
+        case "RESET":
+            return{
+                ...state,
+                myFavorites: state.allCharacters,
+            }
         default:
             return {...state};
     }
